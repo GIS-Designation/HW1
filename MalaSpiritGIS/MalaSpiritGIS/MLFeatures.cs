7140953592efb57b17e3a436528d180b8d5d7ae3
kuuhakuJ <33505995+kuuhakuJ@users.noreply.github.com>
2020-05-23 16:24:28 +0800

    创建项目代码
    定义了部分基础的要素类

---
+++ MalaSpiritGIS/MalaSpiritGIS/MLFeatures.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Drawing;
+
+namespace MalaSpiritGIS
+{
+    /// <summary>
+    /// 要素类型枚举，点，多段线，多边形，多点
+    /// </summary>
+    enum FeatureType { POINT,POLYLINE,POLYGON,MULTIPOINT}
+
+    /// <summary>
+    /// double类型点坐标
+    /// </summary>
+    class PointD
+    {
+        private double x, y;
+
+        #region 构造函数
+        /// <summary>
+        /// 基于坐标生成点
+        /// </summary>
+        /// <param name="_x">横坐标</param>
+        /// <param name="_y">纵坐标</param>
+        public PointD(double _x, double _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        #endregion
+
+        public double X { get { return x; } set { x = value; } }
+
+        public double Y { get { return y; } set { y = value; } }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) { return false; }
+            if (obj.GetType().Equals(this.GetType()) == false) { return false; }
+            PointD pt = (PointD)obj;
+            return Math.Abs(x - pt.x) < Math.Abs(x + pt.x) * 1e-15 && Math.Abs(y - pt.y) < Math.Abs(y + pt.y) * 1e-15;
+        }
+
+        public override int GetHashCode()
+        {
+            return x.GetHashCode() + y.GetHashCode();
+        }
+    }
+
+    /// <summary>
+    /// 多段线
+    /// </summary>
+    class PolylineD
+    {
+        List<PointD> points;
+
+        #region 构造函数
+        /// <summary>
+        /// 基于点数组构造多段线
+        /// </summary>
+        /// <param name="_points"></param>
+        public PolylineD(PointD[] _points)
+        {
+            points = new List<PointD>(_points);
+        }
+        #endregion
+
+        public int Count { get { return points.Count; } }
+
+        public PointD GetPoint(int index)
+        {
+            return points[index];
+        }
+
+        public void AddPoint(PointD point)
+        {
+            points.Add(point);
+        }
+
+        public bool IsClose()
+        {
+            return points.Count >= 3 && points[0] == points[points.Count - 1];
+        }
+    }
+
+    /// <summary>
+    /// 多边形
+    /// </summary>
+    class PolygonD
+    {
+        List<PolylineD> rings;
+
+        #region 构造函数
+        /// <summary>
+        /// 基于多环构造多边形
+        /// </summary>
+        /// <param name="_rings"></param>
+        public PolygonD(PolylineD[] _rings)
+        {
+            rings = new List<PolylineD>(_rings);
+        }
+        #endregion
+
+        public int Count { get { return rings.Count; } }
+
+        public PolylineD GetRing(int index)
+        {
+            return rings[index];
+        }
+
+        public void AddRing(PolylineD ring)
+        {
+            rings.Add(ring);
+        }
+    }
+
+    /// <summary>
+    /// 麻辣精灵要素抽象基类
+    /// </summary>
+    abstract class MLFeature
+    {
+        #region 属性
+        protected int id;                     //要素编号
+        protected FeatureType featureType;    //要素类型
+        protected double[] mbr;               //要素最小外包矩形，xmin，xmax，ymin，ymax
+        protected int pointNum;               //要素包含点的数量
+        protected DataTable attributeData;    //要素的属性表
+        protected PointF deviation;           //要素在屏幕绘制的偏移值
+        #endregion
+
+        static int count=0;
+
+        public MLFeature()
+        {
+            id = ++count;   //id自动+1
+            mbr = new double[4];
+            deviation = new PointF();
+        }
+
+        //用于剪切，复制，粘贴等操作
+        public abstract byte[] ToBytes();
+
+        public void Move(float x,float y)
+        {
+            deviation.X += x;
+            deviation.Y += y;
+        }
+    }
+
+    class MLPoint : MLFeature
+    {
+        PointD point;
+
+        public MLPoint(PointD _p) : base()//TODO: 应该是函数体内进行sql查询，直接获取数据进行构造
+        {
+            point = new PointD(_p.X, _p.Y);
+            featureType = FeatureType.POINT;
+            mbr[0] = mbr[1] = point.X;
+            mbr[2] = mbr[3] = point.Y;
+            pointNum = 1;
+            //TODO: FillDataTable
+        }
+
+        public override byte[] ToBytes()
+        {
+            byte[] rslt=new byte[1];
+            return rslt;
+        }
+    }
+
+}
---
+++ MalaSpiritGIS/MalaSpiritGIS/MLMainForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MalaSpiritGIS
+{
+    partial class MLMainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.文件FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件FToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // 文件FToolStripMenuItem
+            // 
+            this.文件FToolStripMenuItem.Name = "文件FToolStripMenuItem";
+            this.文件FToolStripMenuItem.Size = new System.Drawing.Size(58, 21);
+            this.文件FToolStripMenuItem.Text = "文件(&F)";
+            // 
+            // MLMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MLMainForm";
+            this.Text = "麻辣精灵GIS";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件FToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
+
