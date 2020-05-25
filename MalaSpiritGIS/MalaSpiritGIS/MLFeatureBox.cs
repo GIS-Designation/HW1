f66ec9e4bcc31b0c38cae3693cd069394c428e1e
李法承 <1700012467@pku.edu.cn>
2020-05-26 00:03:45 +0800

    将类变成了public，新加了一个控件

---
+++ MalaSpiritGIS/MalaSpiritGIS/MLFeatureBox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MalaSpiritGIS
+{
+    public partial class MLFeatureBox : UserControl
+    {
+        public MLFeatureBox()
+        {
+            InitializeComponent();
+
+            addFeatureClass();// 测试
+        }
+
+        #region 属性
+        List<MLFeatureClass> layers;
+        #endregion
+        /// <summary>
+        /// 添加一个图层，仅供展示，功能还未具体制作
+        /// </summary>
+        public void addFeatureClass()//MLFeatureClass layer)
+        {
+            Label label = new Label();
+            label.Location = new Point(3, 30);
+            label.Text = "testtt";
+            Controls.Add(label);
+            label.MouseClick += new System.Windows.Forms.MouseEventHandler(showLayerMenu);
+        }
+
+        private void showBoxMenu(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                boxMenu.Show(MousePosition.X, MousePosition.Y);
+            }
+        }
+
+        private void showLayerMenu(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                layerMenu.Show(MousePosition.X, MousePosition.Y);
+            }
+        }
+    }
+}
--- MalaSpiritGIS/MalaSpiritGIS/MLMainForm.Designer.cs
+++ MalaSpiritGIS/MalaSpiritGIS/MLMainForm.Designer.cs
@@ -29,10 +29,11 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.文件FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mlFeatureBox1 = new MalaSpiritGIS.MLFeatureBox();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -46,6 +47,12 @@
             this.menuStrip1.Size = new System.Drawing.Size(800, 25);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件FToolStripMenuItem
+            // 
+            this.文件FToolStripMenuItem.Name = "文件FToolStripMenuItem";
+            this.文件FToolStripMenuItem.Size = new System.Drawing.Size(58, 21);
+            this.文件FToolStripMenuItem.Text = "文件(&F)";
             // 
             // statusStrip1
             // 
@@ -70,17 +77,22 @@
             this.toolStripStatusLabel2.Size = new System.Drawing.Size(131, 17);
             this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
             // 
-            // 文件FToolStripMenuItem
+            // mlFeatureBox1
             // 
-            this.文件FToolStripMenuItem.Name = "文件FToolStripMenuItem";
-            this.文件FToolStripMenuItem.Size = new System.Drawing.Size(58, 21);
-            this.文件FToolStripMenuItem.Text = "文件(&F)";
+            this.mlFeatureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mlFeatureBox1.Location = new System.Drawing.Point(0, 28);
+            this.mlFeatureBox1.MaximumSize = new System.Drawing.Size(90, 1080);
+            this.mlFeatureBox1.MinimumSize = new System.Drawing.Size(90, 2);
+            this.mlFeatureBox1.Name = "mlFeatureBox1";
+            this.mlFeatureBox1.Size = new System.Drawing.Size(90, 397);
+            this.mlFeatureBox1.TabIndex = 2;
             // 
             // MLMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mlFeatureBox1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -102,6 +114,7 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private MLFeatureBox mlFeatureBox1;
     }
 }
 
