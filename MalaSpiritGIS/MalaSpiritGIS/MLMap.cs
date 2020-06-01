a7d8f0ebacbcf8fffe3ec3c57760a1cbd9b98bf8
李法承 <1700012467@pku.edu.cn>
2020-06-02 06:00:28 +0800

    Revert "紧急修复了编辑节点倒置bug"
    This reverts commit 9d1d98cec2a918d2d4daa16ca54c48baf4268e0d.

--- MalaSpiritGIS/MalaSpiritGIS/MLMap.cs
+++ MalaSpiritGIS/MalaSpiritGIS/MLMap.cs
@@ -1067,7 +1067,7 @@ namespace MalaSpiritGIS
                     {
                         if(EditPoint.p != null)
                         {
-                            EditPoint.p.Move(e.Location.X - startPoint.X, startPoint.Y - e.Location.Y);
+                            EditPoint.p.Move(e.Location.X - startPoint.X, e.Location.Y - startPoint.Y);
                             startPoint = e.Location;
                             Refresh();
                         }
