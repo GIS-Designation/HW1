76a019a247418ad5eba92ed842a401750d4b2482
mahancong <mahancong@pku.edu.cn>
2020-06-01 20:22:20 +0800

    修改条件查询

--- MalaSpiritGIS/MalaSpiritGIS/AttributeTable.Designer.cs
+++ MalaSpiritGIS/MalaSpiritGIS/AttributeTable.Designer.cs
@@ -32,7 +32,6 @@
             this.增加字段ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.开始编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.停止编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.按属性查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.全部选择ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.取消选择ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.attributeView = new System.Windows.Forms.DataGridView();
@@ -42,59 +41,53 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.增加字段ToolStripMenuItem,
             this.开始编辑ToolStripMenuItem,
             this.停止编辑ToolStripMenuItem,
-            this.按属性查询ToolStripMenuItem,
             this.全部选择ToolStripMenuItem,
             this.取消选择ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(600, 25);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 4, 0, 4);
+            this.menuStrip1.Size = new System.Drawing.Size(1200, 43);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // 增加字段ToolStripMenuItem
             // 
             this.增加字段ToolStripMenuItem.Name = "增加字段ToolStripMenuItem";
-            this.增加字段ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.增加字段ToolStripMenuItem.Size = new System.Drawing.Size(130, 35);
             this.增加字段ToolStripMenuItem.Text = "增加字段";
             this.增加字段ToolStripMenuItem.Click += new System.EventHandler(this.增加字段ToolStripMenuItem_Click);
             // 
             // 开始编辑ToolStripMenuItem
             // 
             this.开始编辑ToolStripMenuItem.Name = "开始编辑ToolStripMenuItem";
-            this.开始编辑ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.开始编辑ToolStripMenuItem.Size = new System.Drawing.Size(130, 35);
             this.开始编辑ToolStripMenuItem.Text = "开始编辑";
             this.开始编辑ToolStripMenuItem.Click += new System.EventHandler(this.开始编辑ToolStripMenuItem_Click);
             // 
             // 停止编辑ToolStripMenuItem
             // 
             this.停止编辑ToolStripMenuItem.Name = "停止编辑ToolStripMenuItem";
-            this.停止编辑ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.停止编辑ToolStripMenuItem.Size = new System.Drawing.Size(130, 35);
             this.停止编辑ToolStripMenuItem.Text = "停止编辑";
             this.停止编辑ToolStripMenuItem.Click += new System.EventHandler(this.停止编辑ToolStripMenuItem_Click);
-            // 
-            // 按属性查询ToolStripMenuItem
-            // 
-            this.按属性查询ToolStripMenuItem.Name = "按属性查询ToolStripMenuItem";
-            this.按属性查询ToolStripMenuItem.Size = new System.Drawing.Size(80, 21);
-            this.按属性查询ToolStripMenuItem.Text = "按属性查询";
             // 
             // 全部选择ToolStripMenuItem
             // 
             this.全部选择ToolStripMenuItem.Name = "全部选择ToolStripMenuItem";
-            this.全部选择ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.全部选择ToolStripMenuItem.Size = new System.Drawing.Size(130, 35);
             this.全部选择ToolStripMenuItem.Text = "全部选择";
             this.全部选择ToolStripMenuItem.Click += new System.EventHandler(this.全部选择ToolStripMenuItem_Click);
             // 
             // 取消选择ToolStripMenuItem
             // 
             this.取消选择ToolStripMenuItem.Name = "取消选择ToolStripMenuItem";
-            this.取消选择ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.取消选择ToolStripMenuItem.Size = new System.Drawing.Size(130, 35);
             this.取消选择ToolStripMenuItem.Text = "取消选择";
             this.取消选择ToolStripMenuItem.Click += new System.EventHandler(this.取消选择ToolStripMenuItem_Click);
             // 
@@ -111,12 +104,12 @@
             this.attributeView.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
             this.attributeView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.attributeView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.attributeView.Location = new System.Drawing.Point(0, 25);
-            this.attributeView.Margin = new System.Windows.Forms.Padding(2);
+            this.attributeView.Location = new System.Drawing.Point(0, 50);
+            this.attributeView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.attributeView.Name = "attributeView";
             this.attributeView.RowHeadersWidth = 51;
             this.attributeView.RowTemplate.Height = 27;
-            this.attributeView.Size = new System.Drawing.Size(600, 334);
+            this.attributeView.Size = new System.Drawing.Size(1200, 668);
             this.attributeView.TabIndex = 1;
             this.attributeView.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.attributeView_CellMouseDown);
             this.attributeView.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.attributeView_CellMouseUp);
@@ -124,13 +117,13 @@
             // 
             // AttributeTable
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.ClientSize = new System.Drawing.Size(1200, 720);
             this.Controls.Add(this.attributeView);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AttributeTable";
             this.Text = "AttributeTable";
             this.menuStrip1.ResumeLayout(false);
@@ -148,7 +141,6 @@
         private System.Windows.Forms.DataGridView attributeView;
         private System.Windows.Forms.ToolStripMenuItem 开始编辑ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 停止编辑ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 按属性查询ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 全部选择ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 取消选择ToolStripMenuItem;
     }
