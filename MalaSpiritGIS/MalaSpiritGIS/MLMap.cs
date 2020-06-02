cdad96d16f137603371c910531b9b8e5bd6f58d6
Joseph ine <718828325@qq.com>
2020-06-02 09:04:39 +0800

    null

--- MalaSpiritGIS/MalaSpiritGIS/MLMap.cs
+++ MalaSpiritGIS/MalaSpiritGIS/MLMap.cs
@@ -1066,7 +1066,7 @@ namespace MalaSpiritGIS
                     {
                         if(EditPoint.p != null)
                         {
-                            EditPoint.p.Move(e.Location.X - startPoint.X, e.Location.Y - startPoint.Y);
+                            EditPoint.p.Move(e.Location.X - startPoint.X,startPoint.Y - e.Location.Y);
                             startPoint = e.Location;
                             Refresh();
                         }
